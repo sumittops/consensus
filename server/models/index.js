48753522f768ed505b1eaeb05094c10355da5b51
Sumit Majumdar <sumit.majumdar@travenues.com>
2020-05-19 01:59:55 +0530

    start server with db connection retry

--- server/models/index.js
+++ server/models/index.js
@@ -4,13 +4,38 @@ const User = require('./user');
 const Debate = require('./debate');
 const VideoCall = require('./videoCall');
 
+function getConnection(retries = 1) {
+    let attempts = 1;
+    return new Promise((resolve, reject) => {
+        function establishConnection(n) {
+            if (n == 0) {
+                reject(new Error('Unable to connect to db'));
+            }
+            console.log('connection attempt #', attempts);
+            mongoose.connect(
+                process.env.DATABASE_URL,
+                { useNewUrlParser: true }
+            ).then(connection => {
+                console.log('db connected');
+                resolve(connection);
+            }).catch(() => {
+                if (n > 0) {
+                    attempts++;
+                    console.log('retrying db connection');
+                    setTimeout(() => {
+                        establishConnection(n - 1)
+                    }, attempts * 2000)
+                }
+            });
+        }
 
-const connectDb = () => {
+        return establishConnection(retries)
+    });
+}
+
+const connectDb = async () => {
     if (process.env.DATABASE_URL) {
-        return mongoose.connect(
-            process.env.DATABASE_URL,
-            { useNewUrlParser: true }
-        )
+        return await getConnection(10)
     }
     return Promise.reject('DB connection unavailable');
 };
